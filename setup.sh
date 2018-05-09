361640b3c00fb05dba57d348e89b78cbaeb6a780
malik lakhani <maliklakhani67@gmail.com>
2018-05-09 10:32:05 +0530

    setup and docker compose file added for setup mini flux

---
+++ setup.sh
@@ -0,0 +1,13 @@
+export MINIFLUX_PORT=80
+export POSTGRES_USER=miniflux
+export POSTGRES_PASSWORD=JMLEwQHKG7
+export POSTGRES_PORT=5432
+export POSTGRES_DB=miniflux
+
+if [ -z $1 ]; then
+  docker-compose up -d
+elif [[ $1 = "up" ]]; then
+  docker-compose up --build
+elif [[ $1 = "down" ]]; then
+  docker-compose down
+fi
