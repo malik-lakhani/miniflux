561cbaf5ca732203f7b9ffacd07499d986593bfa
malik lakhani <maliklakhani67@gmail.com>
2018-05-10 12:05:41 +0530

    get feed entries and return matched entries into css

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const request = require('request');
+const request = require('request-promise');
 const csv = require("fast-csv");
 const fs = require('fs');
+const htmlToText = require('html-to-text');
 
 const result = require('dotenv').config()
 
@@ -15,34 +16,83 @@ const password = process.env.PASSWORD;
 
 const regexPattern = process.argv[2] || /([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})/;
 
+/*
+====================================================================================================
+
+return all Feeds entries
+
+====================================================================================================
+*/
 const getAllFeeds = () => {
-  return new Promise((resolve) => {
-    request.get(`${baseUrl}/${apiVersion}/feeds`, {
-      'auth': {
-        'user': user,
-        'pass': password,
-      }
-    }, function(err, res) {
-      if (err) {
-        return reject(err);
-      }
-      return resolve(JSON.parse(res.body));
-    });
+  const option = {
+    uri: `${baseUrl}/${apiVersion}/feeds`,
+    'auth': {
+      'user': user,
+      'pass': password,
+    }
+  }
+  return request.get(option)
+  .then((response) => {
+    return JSON.parse(response);
+  })
+  .then((feedsData) => {
+    return Promise.all(feedsData.map((feedTemp) => {
+      return getFeedEntries(feedTemp)
+    })
+  )
+  .then((feedEntries) => {
+    const flatten = (arr) => {
+      return arr.reduce((flat, toFlatten) => {
+        return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+      }, []);
+    }
+      return flatten(feedEntries);
+    })
   })
 }
+
+/*
+====================================================================================================
+
+return all Feeds entries for given feed
+
+====================================================================================================
+*/
+const getFeedEntries = (feedTemp) => {
+  const option = {
+    uri: `${baseUrl}/${apiVersion}/feeds/${feedTemp.id}/entries`,
+    'auth': {
+      'user': user,
+      'pass': password,
+    }
+  }
+  return request.get(option)
+  .then((response) => {
+    return JSON.parse(response).entries;
+  })
+}
+
+/*
+====================================================================================================
+
+return all Feeds entries which match the given pattern.
+
+====================================================================================================
+*/
 const getMatchedFeeds = (pattern) => {
   const regex = new RegExp(pattern,"i")
   return getAllFeeds()
   .then((feeds) => {
+      console.log(feeds)
     let matchedFeeds = feeds.filter((feed) => {
-      return ((feed.title && feed.title.match(regex)) || (feed.description && feed.description.match(regex)))
+      return ((feed.title && feed.title.match(regex)) || (feed.content && htmlToText.fromString(feed.content).match(regex)))
     }).map((feed) => {
       let matchedWords = [];
       feed.title && feed.title.match(regex) ? matchedWords.push(feed.title.match(regex)[0]) : undefined;
-      feed.description && feed.description.match(regex) ? matchedWords.push(feed.description.match(regex)[0]) : undefined;
+      feed.content && htmlToText.fromString(feed.content).match(regex) ? matchedWords.push(htmlToText.fromString(feed.content).match(regex)[0]) : undefined;
       return {
         title: feed.title,
-        feed_url: feed.feed_url,
+        feed_url: feed.url,
         matched_words: matchedWords ? matchedWords[0].toString() : ''
       }
     })
@@ -51,6 +101,13 @@ const getMatchedFeeds = (pattern) => {
 
 }
 
+/*
+====================================================================================================
+
+Wtite feeds data to csv
+
+====================================================================================================
+*/
 const writeToCsv = (totalFeeds) => {
   return new Promise((resolve, reject) => {
     csv
@@ -70,6 +127,13 @@ const writeToCsv = (totalFeeds) => {
   })
 }
 
+/*
+====================================================================================================
+
+Get matched feed entries and write data to csv.
+
+====================================================================================================
+*/
 getMatchedFeeds(regexPattern)
 .then((filteredFeeds) => {
   if (!filteredFeeds || !filteredFeeds.length) {
